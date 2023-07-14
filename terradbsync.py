6861a61ab6498a11c5c3f1046555dbf8f1d8a46d
Zyd8 <115306019+Zyd8@users.noreply.github.com>
2023-07-15 07:22:47 +0800

    move the entry point .py file to repo root path

--- terradbsync.py
+++ terradbsync.py
@@ -1,26 +1,8 @@
-from backup import Backup
-from path import Path
-from setup import Setup
-from sync import Sync
-
-
-def set_backups():
-    """make "backups" directory if not already created, else, create"""
-    Backup.set_android_dir(Path.ANDROID.get_terraria_backup_rootpath())
-    Backup.set_pc_dir(Backup.current_pc_os.get_terraria_backup_rootpath())
-
-
-def set_pc_android():
-    """Identify PC os, check adb directory and initialize adb connection"""
-    Sync.check_pc_os()
-    Sync.check_adb_dir()
-    Sync.check_adb_connection()
-
-
-def set_rootpaths():
-    """Find default Terraria directories, else, initialize custom paths"""
-    Path.set_pc_terraria_rootpath()
-    Path.set_android_terraria_rootpath()
+from src.backup import Backup
+from src.errorhandler import ErrorHandler
+from src.path import Path
+from src.setup import Setup
+from src.sync import Sync
 
 
 def check_subpaths():
@@ -48,6 +30,25 @@ def do_backup_sync():
     Backup.remove_old_archives()
 
 
+def set_backups():
+    """make "backups" directory if not already created, else, create"""
+    Backup.set_android_dir(Path.ANDROID.get_terraria_backup_rootpath())
+    Backup.set_pc_dir(Backup.current_pc_os.get_terraria_backup_rootpath())
+
+
+def set_pc_android():
+    """Identify PC os, check adb directory and initialize adb connection"""
+    Sync.check_pc_os()
+    Sync.check_adb_dir()
+    Sync.check_adb_connection()
+
+
+def set_rootpaths():
+    """Find default Terraria directories, else, initialize custom paths"""
+    Path.set_pc_terraria_rootpath()
+    Path.set_android_terraria_rootpath()
+
+
 def main():
 
     print("=====Setup start====")
@@ -59,6 +60,7 @@ def main():
     
     do_backup_sync()
     print("TerrADBsync Complete!")
+    ErrorHandler.no_error_terminate()
 
 
 if __name__ == "__main__":
